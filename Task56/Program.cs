b524204b7bdb00cd7665be92d626c473542bbf9f
Kovalev Alex <k9037837817@gmail.com>
2023-03-03 15:01:51 +0300

    Задачи 54, 56, 58, 60, 62

---
+++ Task56/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задача 56: Задайте прямоугольный двумерный массив. Напишите
+// программу, которая будет находить строку с наименьшей суммой элементов.
+
+
+int [,] GetArray(int rows, int columns)
+{
+    int [,] array = new int[rows, columns];
+    Random arrayRandomValue = new Random();
+        for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = arrayRandomValue.Next(0, 10);
+                }
+            }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+
+        {
+            Console.Write("{0,6}", array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetArrayRowsMinSum (int [,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) sumRow += array[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.WriteLine($"Строка с наименьшей суммой элементов это строка под № {minSumRow + 1}");
+}
+
+int [,] array = GetArray(8,3);
+PrintArray(array);
+Console.WriteLine();
+GetArrayRowsMinSum(array);
---
+++ Task60/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 60: Сформируйте трёхмерный массив из двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+int[,,] array = new int[2, 2, 2];
+
+void PrintIndex(int[,,] arr)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+void FillArrayRandom(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = new Random().Next(10, 100);
+            }
+        }
+
+    }
+}
+
+FillArrayRandom(array);
+PrintIndex(array);
